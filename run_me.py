93776bc38d8ab48c194e3dd1c63bc3a8a8268458
Abdullah Akram Ansari <ansari90@outlook.com>
2020-01-12 19:47:54 -0500

    trig wave printer

--- run_me.py
+++ run_me.py
@@ -1,4 +1,15 @@
+import time
 from math import sin, cos, radians
 
-for i in range(360):
-    print(cos(radians(i)))
+# Create a string with spaces proportional to a cosine of x in degrees
+def make_cos_dot_string(x):
+    return '>' * int(10 * cos(radians(x)) + 20) + '0'
+
+def make_sin_dot_string(x):
+    return '<' * int(10 * sin(radians(x)) + 20) + '1'
+
+
+for i in range(0, 3600, 18):
+    print(make_cos_dot_string(i))
+    print(make_sin_dot_string(i))
+    time.sleep(0.05)
